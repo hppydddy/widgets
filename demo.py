b1362351ad61ad7b850147a5f8e2344133684d20
hppydddy <143551124+hppydddy@users.noreply.github.com>
2023-08-29 22:23:46 +0200

    Camera included

--- demo.py
+++ demo.py
@@ -4,6 +4,11 @@ import numpy as np
 
 st.title('Mini App')
 
+picture = st.camera_input("Take a picture")
+
+if picture:
+    st.image(picture)
+
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
